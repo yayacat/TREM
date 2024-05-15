3118650a3011284cc5a995d2d484420507ffa87b
夜夜 <vcde0000@gmail.com>
2024-05-15 16:26:06 +0800

    6.1.107

--- TREM.Electron/js/earthquake.js
+++ TREM.Electron/js/earthquake.js
@@ -4181,20 +4181,35 @@ function openFileWindow() {
 
 ipcRenderer.on("readReplayFile", (event, filePaths) => {
 	try {
+		const MAX_FILES = 10000;
+		let fileCount = 0;
+
 		fs.readFile(filePaths[0], async (err, deta) => {
 			if (err) throw err;
 			await JSZip.loadAsync(deta).then(async (zip) => {
 				const replayData = [];
+				zip.forEach((relativePath, zipEntry) => {
+					fileCount++;
 
-				for (let i = 0, k = Object.keys(zip.files), n = k.length; i < n; i++) {
-					const filename = k[i];
-					const content = await zip.files[filename].async("string");
-					const data = JSON.parse(content);
+					if (fileCount > MAX_FILES)
+						throw "Reached max. number of files";
+
+					const data = JSON.parse(zipEntry.async("string"));
 					data.rts.replay = true;
 					data.eew.forEach((e) => (e.replay = true));
 					data.time = +filename;
 					replayData.push(data);
-				}
+				});
+
+				// for (let i = 0, k = Object.keys(zip.files), n = k.length; i < n; i++) {
+				// 	const filename = k[i];
+				// 	const content = await zip.files[filename].async("string");
+				// 	const data = JSON.parse(content);
+				// 	data.rts.replay = true;
+				// 	data.eew.forEach((e) => (e.replay = true));
+				// 	data.time = +filename;
+				// 	replayData.push(data);
+				// }
 
 				const replayLength = replayData.length;
 				let replayPercentage = 0;
